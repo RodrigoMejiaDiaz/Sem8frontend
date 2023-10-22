d87bd36925c18cfdce9c5ffd22eca5c94a890255
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2023-10-22 17:28:09 -0500

    3

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,10 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      ></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
